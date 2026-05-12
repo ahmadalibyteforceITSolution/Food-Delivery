5f9c0076a4b5704ae27a1d3fa381e3a3e981700b
AhmadAli <https://github.com/ahmadalibyteforceITSolution>
2026-05-12 07:29:45 -0700

    initail

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import serverless from 'serverless-http';
 import { Resend } from 'resend';
 dotenv.config();
