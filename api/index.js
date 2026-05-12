20742912097d0a69d1267d6c78c0d7df8f73d184
AhmadAli <https://github.com/ahmadalibyteforceITSolution>
2026-05-12 08:19:44 -0700

    Final cleanup: removed redundant backend folder, unified API structure, and optimized Vercel configuration

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-// import serverless from 'serverless-http';
 import { Resend } from 'resend';
 dotenv.config();
 
